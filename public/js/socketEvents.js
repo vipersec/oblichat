9147e4653419e70d5bdc370c9b0ad2c7dd7d5295
Stavros Moiras <steve@vipersec.com>
2017-02-07 18:12:22 +0100

    Show loader during key generation

--- public/js/socketEvents.js
+++ public/js/socketEvents.js
@@ -10,8 +10,10 @@ function socketConnect() {
     if($('#upperbar').is(':visible')){
 
         if (!socket.connected) {
+
             console.log("socket connect");
             socket.open(); // start connecting
+            showSpinner();
         }
 
     }
@@ -21,11 +23,44 @@ function socketConnect() {
 }
 
 /**
+ * Helper function that shows a a loading spinner
+ */
+function showSpinner() {
+    var opts = {
+        lines: 9 // The number of lines to draw
+        , length: 0 // The length of each line
+        , width: 30 // The line thickness
+        , radius: 54 // The radius of the inner circle
+        , scale: 1.5 // Scales overall size of the spinner
+        , corners: 1 // Corner roundness (0..1)
+        , color: '#000' // #rgb or #rrggbb or array of colors
+        , opacity: 0.1 // Opacity of the lines
+        , rotate: 0 // The rotation offset
+        , direction: 1 // 1: clockwise, -1: counterclockwise
+        , speed: 0.9 // Rounds per second
+        , trail: 41 // Afterglow percentage
+        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+        , zIndex: 2e9 // The z-index (defaults to 2000000000)
+        , className: 'spinner' // The CSS class to assign to the spinner
+        , top: '50%' // Top position relative to parent
+        , left: '50%' // Left position relative to parent
+        , shadow: false // Whether to render a shadow
+        , hwaccel: false // Whether to use hardware acceleration
+        , position: 'absolute' // Element positioning
+    };
+    var target = document.getElementById('spinner');
+    var spinner = new Spinner(opts).spin(target);
+    $(".spinner").css({"top": "30%"});
+    $("#loader").show();
+}
+
+/**
  * Function that executes after the RSA key pair has been generated and is stored on the server.
  * it hides the loading icon and emits to our contacts that we are online
  */
 function loaded() {
     socket.emit('loaded');
+    $("#loader").fadeOut("slow");
     console.log("ready!!!");
 }
 

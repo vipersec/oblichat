8d2e37b95165939262be842a75c8c605e8f6836b
Stavros Moiras <steve@vipersec.com>
2017-02-13 19:22:17 +0100

    Fixed some UI problems

--- public/js/socketEvents.js
+++ public/js/socketEvents.js
@@ -42,11 +42,10 @@ function showSpinner() {
         , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
         , zIndex: 2e9 // The z-index (defaults to 2000000000)
         , className: 'spinner' // The CSS class to assign to the spinner
-        , top: '40%' // Top position relative to parent
         , left: '50%' // Left position relative to parent
         , shadow: false // Whether to render a shadow
         , hwaccel: false // Whether to use hardware acceleration
-        , position: 'absolute' // Element positioning
+        , position: 'relative' // Element positioning
     };
     var target = document.getElementById('spinner');
     var spinner = new Spinner(opts).spin(target);
@@ -59,7 +58,7 @@ function showSpinner() {
  */
 function loaded() {
     socket.emit('loaded');
-    $("#loader").fadeOut("slow");
+    $("#loader").fadeOut();
     console.log("ready!!!");
 }
 
